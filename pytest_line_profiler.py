4d05cc2af56eb21a2bb7169a0c480e05a1747484
Martín Gaitán <gaitan@gmail.com>
2021-05-03 10:09:47 -0300

    working as mark / argument

--- pytest_line_profiler.py
+++ pytest_line_profiler.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
 
+import io
 import pytest
 from importlib import import_module
 from line_profiler import LineProfiler
+
+
+def get_stats(lp: LineProfiler) -> str:
+    s = io.StringIO()
+    lp.print_stats(stream=s)
+    return s.getvalue()
 
 
 def import_string(dotted_path):
     """
     Import a dotted module path and return the object if is callable
     """
+    if not isinstance(dotted_path, str):
+        return dotted_path
     try:
         module_path, callable_name = dotted_path.rsplit('.', 1)
         module = import_module(module_path)
@@ -25,7 +34,7 @@ def pytest_addoption(parser):
         '--line-profile',
         action='store',
         nargs="*",
-        help='Set the value for the fixture "bar".'
+        help='Register a function to profile while executed tests.'
     )
 
     
@@ -39,24 +48,34 @@ def pytest_load_initial_conftests(early_config, parser, args):
 
 def pytest_runtest_call(item):
     instrumented = []
-    import ipdb; ipdb.set_trace()
     if item.get_closest_marker("line_profile"):
-        instrumented += item.get_closest_marker("line_profile").args
+        instrumented += [import_string(s) for s in item.get_closest_marker("line_profile").args]
     if item.config.getvalue("line_profile"):
         instrumented += [import_string(s) for s in item.config.getvalue("line_profile")]
     
     if instrumented:
         lp = LineProfiler(*instrumented)
         lp.runcall(item.runtest)
-        item.addfinalizer(lp.print_stats)
+        item.config._line_profile = getattr(item.config, "_line_profile", {})
+        item.config._line_profile[item.nodeid] = get_stats(lp)
     else:
         item.runtest()
 
 
-@pytest.fixture
-def line_profiler(request,):
-    # return request.config.option.dest_foo
 
+def pytest_terminal_summary(
+    terminalreporter: "TerminalReporter",
+    exitstatus: "ExitCode",
+    config: "Config",
+) -> None:
+    reports = getattr(config, "_line_profile", {})
+    for k, v in reports.items():
+        terminalreporter.write_sep("=", f"Line Profile result for {k}")
+        terminalreporter.write(v)
+
+
+@pytest.fixture
+def line_profiler(request):
     lp = LineProfiler()
     yield lp
     request.addfinalizer(lp.print_stats)
--- tests/test_line_profiler.py
+++ tests/test_line_profiler.py
@@ -1,22 +1,13 @@
 # -*- coding: utf-8 -*-
 import pytest
-from math import sin 
 
 def f(i):
     return i * 10
 
-def g():
+def g(n=10):
     return sum(f(i) for i in range(10))
-
-
 
 
 @pytest.mark.line_profile.with_args(f, g)
 def test_as_mark():
-    g()
-
-
-"""
-def test_via_command_line():
-    sin(1)
-"""
+    assert g() == 450
