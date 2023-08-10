555997afd3536281853fa6a543a409f1e4446c10
Martín Gaitán <gaitan@gmail.com>
2023-08-10 11:14:52 -0300

    bump v0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name='pytest-line-profiler',
-    version='0.1.0',
+    version='0.2.0',
     author='Martín Gaitán',
     author_email='gaitan@gmail.com',
     maintainer='Martín Gaitán',
